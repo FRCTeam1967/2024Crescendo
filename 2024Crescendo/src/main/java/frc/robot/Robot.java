75dbb374ea020825aacffcf12491f44466f19109
shriyankah <shiremagalur27@ndsj.org>
2024-09-17 16:55:52 -0700

    advantagekit/advantagescope

--- 2024Crescendo/src/main/java/frc/robot/Robot.java
+++ 2024Crescendo/src/main/java/frc/robot/Robot.java
@@ -4,9 +4,10 @@
 
 package frc.robot;
 
+import org.littletonrobotics.junction.LoggedRobot;
+
 import edu.wpi.first.wpilibj.DataLogManager;
 import edu.wpi.first.wpilibj.DriverStation;
-import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
@@ -19,7 +20,7 @@ import edu.wpi.first.wpilibj2.command.button.Trigger;
  * the package after creating this project, you must also update the build.gradle file in the
  * project.
  */
-public class Robot extends TimedRobot {
+public class Robot extends LoggedRobot {
   private Command m_autonomousCommand;
 
   private RobotContainer m_robotContainer;
